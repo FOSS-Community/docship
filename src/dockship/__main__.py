e7ad42fe2f795b7777e22ff790c112328611729e
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-12-26 21:52:52 +0530

    feat: make the v0.0.1 of the project

--- src/dockship/__main__.py
+++ src/dockship/__main__.py
@@ -1,2 +1,57 @@
 import argparse
-from dockship import __version__
+from .utils import *
+
+
+def deploy(server_name, app_server_address):
+    print(f"Deploying to server '{server_name}' at address '{app_server_address}'")
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Dockership CLI for managing deployments."
+    )
+
+    subparsers = parser.add_subparsers(
+        title="Commands",
+        dest="command",
+        required=True,
+        help="Available commands",
+    )
+
+    # Deploy command parser
+    deploy_parser = subparsers.add_parser(
+        "deploy", help="Deploy an application to a server"
+    )
+
+    deploy_parser.add_argument(
+        "--server_name",
+        required=True,
+        help="The name of the server where the application will be deployed",
+    )
+
+    deploy_parser.add_argument(
+        "--app_server_address",
+        required=True,
+        help="The address of the application server",
+    )
+
+    args = parser.parse_args()
+
+    if args.command == "deploy":
+        print("Deploying...")
+        print("Checking if Nginx is installed...")
+        is_nginx_install()
+        print("Writing to Nginx configuration...")
+        write_to_nginx_config(args.server_name, args.app_server_address)
+        print("Checking Nginx configuration...")
+        check_nginx_configuration()
+        print("Creating symlink...")
+        create_symlink(args.server_name)
+        print("Restarting Nginx...")
+        restart_nginx_using_systemctl()
+        print("Generating SSL certificate using Certbot...")
+        ssl_using_certbot(args.server_name)
+        print("SSL certificate generated successfully!")
+        print("Deployment successful!")
+
+if __name__ == "__main__":
+    main()
--- src/dockship/utils.py
+++ src/dockship/utils.py
@@ -1,6 +1,15 @@
 import subprocess
 from exceptions import NginxNotInstalled, NginxConfigError
 
+def is_nginx_install() -> None:
+    try:
+        subprocess.run(["nginx", "-version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    except FileNotFoundError:
+        raise NginxNotInstalled("Nginx is not installed. Please install Nginx before running this script.")
+    except Exception:
+        raise Exception("An error occurred while checking if Nginx is installed. This can be due to a permission error or execution error.")
+
+
 def simple_template(server_name: str, app_server_address: str) -> str:
     return """
 server {
@@ -16,18 +25,46 @@ server {
 }  
 """ % (server_name, app_server_address)
 
-def is_nginx_install() -> None:
+def write_to_nginx_config(server_name: str, app_server_address: str) -> None:
     try:
-        subprocess.run(["nginx", "-version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    except FileNotFoundError:
-        raise NginxNotInstalled("Nginx is not installed. Please install Nginx before running this script.")
+        subprocess.run(
+            ["sudo", "tee", f"/etc/nginx/sites-available/{server_name}"],
+            input=simple_template(server_name, app_server_address).encode(),
+            check=True
+        )
+    except subprocess.CalledProcessError:
+        raise Exception("An error occurred while writing to the Nginx configuration file. This can be due to a permission error or execution error.")
     except Exception:
-        raise Exception("An error occurred while checking if Nginx is installed. This can be due to a permission error or execution error.")
+        raise Exception("An error occurred while writing to the Nginx configuration file. This can be due to a permission error or execution error.")
 
-def check_nginx_configured() -> None:
+def check_nginx_configuration() -> None:
     try:
-        subprocess.run(["nginx", "-t"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(["sudo", "nginx", "-t"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     except subprocess.CalledProcessError:
         raise NginxConfigError("There is an error in the Nginx configuration. Please check the configuration and try again.")
     except Exception:
         raise Exception("An error occurred while checking if Nginx is configured. This can be due to a permission error or execution error.")
+
+def create_symlink(server_name: str) -> None:
+    try:
+        subprocess.run(["sudo","ln", "-s", f"/etc/nginx/sites-available/{server_name}", f"/etc/nginx/sites-enabled/{server_name}"], check=True)
+    except subprocess.CalledProcessError:
+        raise Exception("An error occurred while creating a symlink. This can be due to a permission error or execution error.")
+    except Exception:
+        raise Exception("An error occurred while creating a symlink. This can be due to a permission error or execution error.")
+
+def restart_nginx_using_systemctl() -> None:
+    try:
+        subprocess.run(["sudo", "systemctl", "restart", "nginx"], check=True)
+    except subprocess.CalledProcessError:
+        raise Exception("An error occurred while restarting Nginx. This can be due to a permission error or execution error.")
+    except Exception:
+        raise Exception("An error occurred while restarting Nginx. This can be due to a permission error or execution error.")
+    
+def ssl_using_certbot(server_name: str) -> None:
+    try:
+        subprocess.run(["sudo", "certbot", "--nginx", "-d", server_name], check=True)
+    except subprocess.CalledProcessError:
+        raise Exception("An error occurred while setting up SSL using Certbot. This can be due to a permission error or execution error.")
+    except Exception:
+        raise Exception("An error occurred while setting up SSL using Certbot. This can be due to a permission error or execution error.")
